86c1088b06d19a8a628669b60c8273c3047eca6a
Yu Hsien Chiang <yuhsienchiang@Yus-Laptop.local>
2023-04-22 12:51:14 +1000

    Update initializer to use pretrained model config

--- model/bert_encoder.py
+++ model/bert_encoder.py
@@ -1,16 +1,21 @@
 from typing import Tuple
 
 from torch import Tensor as T
-from transformers import BertModel
+from transformers import BertModel, BertConfig
 
 class BertEncoder(BertModel):
     
-    def __init__(self, config, add_pooling_layer=True):
+    def __init__(self, config :BertConfig=None, add_pooling_layer :bool=True) -> None:
 
         super(BertEncoder, self).__init__(config, add_pooling_layer)
         
+        if config is None:
+            super().from_pretrained("bert-base-uncased")
+            
+
         
-    def forward(self, input_ids: T, attention_mask: T, token_type_ids: T, return_dict: bool = True) -> Tuple[T]:
+        
+    def forward(self, input_ids: T, attention_mask: T, token_type_ids: T, return_dict: bool=True) -> Tuple[T]:
         
         output = super().forward(input_ids=input_ids, 
                                  attention_mask=attention_mask, 
