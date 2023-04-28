276ea498d38eb198d4b8c57650620948b91648a7
Yu Hsien Chiang <yuhsienchiang@Yus-Laptop.local>
2023-04-28 15:50:31 +1000

    Update method input to match the new input structure

--- model/biencoder.py
+++ model/biencoder.py
@@ -12,17 +12,17 @@ class BiEncoder(nn.Module):
         self.query_model = query_model
         self.evid_model = evid_model
         
-    def forward(self, query_ids, query_segment, query_attent_mask, evid_ids, evid_segment, evid_attent_mask) -> Tuple[T, T]:
+    def forward(self, query_ids, query_segment, query_attn_mask, evid_ids, evid_segment, evid_attn_mask) -> Tuple[T, T]:
         
         _query_seq, query_pooled_out, _query_hidden = self.get_representation(sub_model=self.query_model,
                                                                               ids=query_ids,
                                                                               segments=query_segment,
-                                                                              attent_mask=query_attent_mask)
+                                                                              attent_mask=query_attn_mask)
         
         _evid_seq, evid_pooled_out, _evid_hidden = self.get_representation(sub_model=self.evid_model,
                                                                            ids=evid_ids,
                                                                            segments=evid_segment,
-                                                                           attent_mask=evid_attent_mask)
+                                                                           attent_mask=evid_attn_mask)
         
         return query_pooled_out, evid_pooled_out
     
