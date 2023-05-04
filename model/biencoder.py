17c3b568c41cccd7f987057bbf2c05befcba6344
Yu Hsien Chiang <yuhsienchiang@Yus-Laptop.local>
2023-05-04 15:56:43 +1000

    fix input dimension issue

--- model/biencoder.py
+++ model/biencoder.py
@@ -14,17 +14,32 @@ class BiEncoder(nn.Module):
         
     def forward(self, query_ids, query_segment, query_attn_mask, evid_ids, evid_segment, evid_attn_mask) -> Tuple[T, T]:
         
-        _query_seq, query_pooled_out, _query_hidden = self.get_representation(sub_model=self.query_model,
+        shape = evid_ids.shape
+        if len(shape) == 3: 
+            batch_size, vec_num, vec_len = shape
+            query_ids = torch.flatten(query_ids, 0, 1)
+            query_segment = torch.flatten(query_segment, 0, 1)
+            query_attn_mask = torch.flatten(query_attn_mask, 0, 1)
+            evid_ids = torch.flatten(evid_ids, 0, 1)
+            evid_segment = torch.flatten(evid_segment, 0, 1)
+            evid_attn_mask = torch.flatten(evid_attn_mask, 0, 1)
+        
+        
+        _query_seq, query_pooler_out, _query_hidden = self.get_representation(sub_model=self.query_model,
                                                                               ids=query_ids,
                                                                               segments=query_segment,
                                                                               attent_mask=query_attn_mask)
         
-        _evid_seq, evid_pooled_out, _evid_hidden = self.get_representation(sub_model=self.evid_model,
+        _evid_seq, evid_pooler_out, _evid_hidden = self.get_representation(sub_model=self.evid_model,
                                                                            ids=evid_ids,
                                                                            segments=evid_segment,
                                                                            attent_mask=evid_attn_mask)
         
-        return query_pooled_out, evid_pooled_out
+        if len(shape) == 3:
+            query_pooled_out = torch.unflatten(query_pooler_out, 0, (batch_size, 1))
+            evid_pooled_out = torch.unflatten(evid_pooler_out, 0, (batch_size, vec_len))
+        
+        return query_pooler_out, evid_pooler_out
     
     def get_representation(self, sub_model: BertEncoder=None, ids: T=None, segments: T=None, attent_mask: T=None) -> Tuple[T, T, T]:
         # make sure the model is add_pooling_layer = True, return_dict=True
@@ -41,7 +56,11 @@ class BiEncoder(nn.Module):
                                 attention_mask=attent_mask)
         
         sequence = out.last_hidden_state
-        pooled_output = out.pooled_output
+        pooler_output = out.pooler_output
         hidden_state = out.hidden_states
         
-        return sequence, pooled_output, hidden_state
+        if sub_model.training:
+            sequence.requires_grad_(requires_grad=True)
+            pooler_output.requires_grad(requires_grad=True)
+        
+        return sequence, pooler_output, hidden_state
