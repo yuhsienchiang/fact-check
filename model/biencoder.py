49c0cca1441755e3bdc8f142043adca9c9041475
Yu Hsien Chiang <yuhsienchiang@Yus-Laptop.local>
2023-05-05 21:09:01 +1000

    modify model to allow accepting only one submodel

--- model/biencoder.py
+++ model/biencoder.py
@@ -6,18 +6,18 @@ from torch import Tensor as T
 from .bert_encoder import BertEncoder
 
 class BiEncoder(nn.Module):
-    def __init__(self, query_model: BertEncoder, evid_model: BertEncoder) -> None:
+    def __init__(self, query_model: BertEncoder, evid_model: BertEncoder=None) -> None:
         super(BiEncoder, self).__init__()
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         
-        self.query_model = query_model.to(device)
-        self.evid_model = evid_model.to(device)
+        self.encoder_0 = query_model.to(device)
+        self.encoder_1 = evid_model.to(device) if evid_model else None
         
     def forward(self, query_ids, query_segment, query_attn_mask, evid_ids, evid_segment, evid_attn_mask) -> Tuple[T, T]:
         
-        shape = evid_ids.shape
-        if len(shape) == 3: 
-            batch_size, vec_num, vec_len = shape
+        input_shape = evid_ids.shape
+        if len(input_shape) == 3: 
+            batch_size, vec_num, _vec_len = input_shape
             query_ids = torch.flatten(query_ids, 0, 1)
             query_segment = torch.flatten(query_segment, 0, 1)
             query_attn_mask = torch.flatten(query_attn_mask, 0, 1)
@@ -26,19 +26,25 @@ class BiEncoder(nn.Module):
             evid_attn_mask = torch.flatten(evid_attn_mask, 0, 1)
         
         
-        _query_seq, query_pooler_out, _query_hidden = self.get_representation(sub_model=self.query_model,
+        _query_seq, query_pooler_out, _query_hidden = self.get_representation(sub_model=self.encoder_0,
                                                                               ids=query_ids,
                                                                               segments=query_segment,
                                                                               attent_mask=query_attn_mask)
         
-        _evid_seq, evid_pooler_out, _evid_hidden = self.get_representation(sub_model=self.evid_model,
-                                                                           ids=evid_ids,
-                                                                           segments=evid_segment,
-                                                                           attent_mask=evid_attn_mask)
+        if self.encoder_1:
+            _evid_seq, evid_pooler_out, _evid_hidden = self.get_representation(sub_model=self.encoder_1,
+                                                                               ids=evid_ids,
+                                                                               segments=evid_segment,
+                                                                               attent_mask=evid_attn_mask)
+        else:
+            _evid_seq, evid_pooler_out, _evid_hidden = self.get_representation(sub_model=self.encoder_0,
+                                                                               ids=evid_ids,
+                                                                               segments=evid_segment,
+                                                                               attent_mask=evid_attn_mask)
         
-        if len(shape) == 3:
-            query_pooled_out = torch.unflatten(query_pooler_out, 0, (batch_size, 1))
-            evid_pooled_out = torch.unflatten(evid_pooler_out, 0, (batch_size, vec_len))
+        if len(input_shape) == 3:
+            query_pooler_out = torch.unflatten(query_pooler_out, 0, (batch_size, 1))
+            evid_pooler_out = torch.unflatten(evid_pooler_out, 0, (batch_size, vec_num))
         
         return query_pooler_out, evid_pooler_out
     
