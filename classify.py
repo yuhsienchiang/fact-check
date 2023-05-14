b145135a159e02250daf59d2306ea5e331964cd0
Sam Chiang <yuhsien.chiang@student.unimelb.edu.au>
2023-05-14 13:37:33 +1000

    optimise code

--- classify.py
+++ classify.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 from transformers import BertTokenizer
 
 from model.classifier.bert_classifier import BertClassifier
@@ -7,10 +8,17 @@ from model.classifier.bert_classifier_trainer import BertClassifierTrainer
 
 
 
-def run(args):
+def run():
     
+    # create tokenizer
+    print("Create tokenizer...")
     bert_tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-    
+    print('\033[1A', end='\x1b[2K')
+    print("Tokenizer created.")
+
+    # Prepare dataset
+    # Training dataset
+    print("Load datasets...")
     classifier_train_dataset = BertClassifierDataset(claim_file_path="./data/train-claims.json",
                                                      data_type="train",
                                                      evidence_file_path="./data/evidence.json",
@@ -18,32 +26,54 @@ def run(args):
                                                      lower_case=False,
                                                      max_padding_length=512,
                                                      rand_seed=666)
+    # Testing dataset
+    classifier_valid_train_dataset = BertClassifierDataset(claim_file_path="./data/train-claims.json",
+                                                           data_type="predict",
+                                                           evidence_file_path="./data/evidence.json",
+                                                           predict_evidence_file_path="./data/output/retrieve-train-claims.json",
+                                                           tokenizer=bert_tokenizer,
+                                                           lower_case=False,
+                                                           max_padding_length=512,
+                                                           rand_seed=666)
     
-    classifier_test_dataset = BertClassifierDataset(claim_file_path="./data/test-claims-unlabelled.json",
-                                                    idata_type="predict",
-                                                    evidence_file_path="./data/evidence.json",
-                                                    predict_evidence_file_path="./data/output/retrieval-claim-prediction.json",
-                                                    tokenizer=bert_tokenizer,
-                                                    lower_case=False,
-                                                    max_padding_length=512,
-                                                    rand_seed=666)
+    classifier_valid_dev_dataset = BertClassifierDataset(claim_file_path="./data/dev-claims.json",
+                                                           data_type="predict",
+                                                           evidence_file_path="./data/evidence.json",
+                                                           predict_evidence_file_path="./data/output/retrieve-dev-claims.json",
+                                                           tokenizer=bert_tokenizer,
+                                                           lower_case=False,
+                                                           max_padding_length=512,
+                                                           rand_seed=666)
     
+    # create model
+    print("Create Bert classifier")
     bert_classifier = BertClassifier()
+    print('\033[1A', end='\x1b[2K')
+    print("Bert classifier created.")
     
+    # train model
+    print("Create model trainer.")
     bert_classifier_trainer = BertClassifierTrainer(bert_classifier,
                                                     batch_size=4)
-    
+    print('\033[1A', end='\x1b[2K')
+    print("Training starts...")
     bert_classifier_trainer.train(train_dataset=classifier_train_dataset,
                                   shuffle=True,
-                                  max_epoch=5,
+                                  max_epoch=20,
                                   loss_func_type="cross_entropy",
                                   optimizer_type="adam",
-                                  learning_rate=0.00001)
+                                  learning_rate=1e-5)
     
-    classification_output= bert_classifier.predict(classifier_train_dataset,
+    # Predict
+    print("Start prediction...")
+    time.sleep(3)
+    print('\033[1A', end='\x1b[2K')
+    classification_output= bert_classifier.predict(classifier_valid_train_dataset,
                                                    batch_size=4,
-                                                   output_file_path=args.predict_output_path)
+                                                   output_file_path="./data/output/classify-train-claims.json")
+    
+    classification_output= bert_classifier.predict(classifier_valid_dev_dataset,
+                                                   batch_size=4,
+                                                   output_file_path="./data/output/classify-dev-claims.json")
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Parameters')
-    parser.add_argument("--predict_output_path", default=None, type=str, help="prediction output file path")
-    args = parser.parse_args()
+    run()
--- retriever.py
+++ retriever.py
@@ -1,5 +1,5 @@
-import argparse
 import time
+from transformers import BertModel
 from transformers import BertTokenizer
 from model.dpr.biencoder_dataset import BiEncoderDataset
 from model.dpr.evidence_dataset import EvidenceDataset
@@ -7,7 +7,7 @@ from model.dpr.biencoder import BiEncoder
 from model.dpr.encoder.bert_encoder import BertEncoder
 from model.dpr.biencoder_trainer import BiEncoderTrainer
 
-def run(args):
+def run():
     
     # create tokenizer
     print("Create tokenizer...")
@@ -25,30 +25,42 @@ def run(args):
                                                tokenizer=bert_tokenizer,
                                                lower_case=False,
                                                max_padding_length=128,
-                                               evidence_num=6,
-                                               rand_seed=5)
+                                               evidence_num=16,
+                                               rand_seed=666)
     # Testing dataset
-    biencoder_test_dataset = BiEncoderDataset(claim_file_path="./data/test-claims-unlabelled.json",
-                                              data_type="predict",
-                                              evidence_file_path="./data/evidence.json",
-                                              tokenizer=bert_tokenizer,
-                                              lower_case=False,
-                                              max_padding_length=128,
-                                              evidence_num=0,
-                                              rand_seed=5)
+    biencoder_valid_train_dataset = BiEncoderDataset(claim_file_path="./data/train-claims.json",
+                                                     data_type="predict",
+                                                     evidence_file_path="./data/evidence.json",
+                                                     tokenizer=bert_tokenizer,
+                                                     lower_case=False,
+                                                     max_padding_length=128,
+                                                     evidence_num=0,
+                                                     rand_seed=666)
+
+    biencoder_valid_dev_dataset = BiEncoderDataset(claim_file_path="./data/dev-claims.json",
+                                                   data_type="predict",
+                                                   evidence_file_path="./data/evidence.json",
+                                                   tokenizer=bert_tokenizer,
+                                                   lower_case=False,
+                                                   max_padding_length=128,
+                                                   evidence_num=0,
+                                                   rand_seed=666)
+    
+    
     #Evidence dataset
     evidence_dataset = EvidenceDataset(evidence_file_path="./data/evidence.json",
                                        tokenizer=bert_tokenizer,
                                        lower_case=False,
                                        max_padding_length=128,
-                                       rand_seed=5)
+                                       rand_seed=666)
+    
     print('\033[1A', end='\x1b[2K')
     print("Datasets loaded.")
     
     # create model
     print("Create BiEncoder...")
-    query_encoder = BertEncoder(add_pooling_layer=True)
-    biencoder = BiEncoder(query_model=query_encoder)
+    query_encoder = BertModel.from_pretrained("bert-base-uncased")
+    biencoder = BiEncoder(query_model=query_encoder, similarity_func_type="dot")
     print('\033[1A', end='\x1b[2K')
     print("BiEncoder created.")
     
@@ -60,32 +72,34 @@ def run(args):
     print("Training starts...")
     biencoder_history = biencoder_trainer.train(train_dataset=biencoder_train_dataset,
                                                 shuffle=True,
-                                                max_epoch=5,
+                                                max_epoch=20,
                                                 loss_func_type="nll_loss",
-                                                similarity_func_type="dot",
                                                 optimizer_type="adam",
-                                                learning_rate=0.0001)
+                                                learning_rate=2e-5)
     
     # Encode evidence
     print("Start embedding evidences....")
     time.sleep(3)
-    evid_embed_data = biencoder.get_evidence_embed(evidence_dataset=evidence_dataset,
+    print("Embedding...")
+    embed_evid_data = biencoder.get_evidence_embed(evidence_dataset=evidence_dataset,
                                                    batch_size=1000,
-                                                   output_file_path=args.embed_output_path)
-
+                                                   output_file_path="./data/output/embed-evidence.json")
+    
     # retrieve info
     print("Start retrieve info...")
     time.sleep(3)
     print('\033[1A', end='\x1b[2K')
-    retrieve_output = biencoder.retrieve(biencoder_test_dataset,
-                                         embed_evid_data=evid_embed_data,
-                                         k=6,
+    retrieve_output = biencoder.retrieve(biencoder_valid_train_dataset,
+                                         embed_evid_data=embed_evid_data,
+                                         k=5,
                                          batch_size =128,
-                                         predict_output_path=args.retrieve_output_path)
+                                         predict_output_path="./data/output/retrieve-train-claims.json")
+    
+    retrieve_output = biencoder.retrieve(biencoder_valid_dev_dataset,
+                                         embed_evid_data=embed_evid_data,
+                                         k=5,
+                                         batch_size =128,
+                                         predict_output_path="./data/output/retrieve-dev-claims.json")
     
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Parameters')
-    parser.add_argument("--embed_output_path", default=None, type=str, help="evidence embedding output file path")
-    parser.add_argument("--retrieve_output_path", default=None, type=str, help="retrieval data output file path")
-    args = parser.parse_args()
-    run(args)
+    run() 
